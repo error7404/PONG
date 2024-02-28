82136369dd542d1451b29fff8b64fd434337008c
error7404 <48163201+error7404@users.noreply.github.com>
2024-02-28 23:26:58 +0100

    added end screen with the winner

--- js/render.js
+++ js/render.js
@@ -3,6 +3,14 @@ import * as THREE from 'three';
 function render(time, p) {
 	if (p.scoreL >= p.rules.maxPoints || p.scoreR >= p.rules.maxPoints)
 	{
+		const filter = document.getElementById('filter');
+		filter.style.opacity = 1;
+		const endScreen = document.getElementById('endScreen');
+		endScreen.style.opacity = 1;
+		if (p.scoreL >= p.rules.maxPoints)
+			endScreen.innerHTML = 'Left player wins!';
+		else
+			endScreen.innerHTML = 'Right player wins!';
 		console.log("the end");
 		return;
 	}
